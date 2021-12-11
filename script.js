3a005de0c5541b7abd5a2d26d73a0848913668ff
Adam (Futur3Sn0w) <futur3sn0w@gmail.com>
2021-12-10 19:13:34 -0500

    Add context menu

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Vars
+
 let navDial = document.getElementById('navDial');
 let showDial = document.getElementById('navActivator');
 
@@ -9,7 +11,13 @@ let button5 = document.getElementById('pageBtn5');
 let button6 = document.getElementById('pageBtn6');
 let button7 = document.getElementById('pageBtn7');
 
+let button = document.querySelector('.pnBtn');
+
 let vwprt = document.getElementById('iViewport');
+
+let infoMenu = document.getElementById('infoMenu');
+
+// NavDial Buttons
 
 navDial.onclick = function () {
     navDial.style.opacity = "0";
@@ -61,4 +69,75 @@ button7.onclick = function() {
     vwprt.src = "subpages/page7.html";
 
     document.getElementById('navActivator').setAttribute("data-pageNo", "7");
+}
+
+// Right-click menu
+
+window.addEventListener('click', function () {
+    infoMenu.style.opacity = "0";
+    infoMenu.style.transform = "scale(0)";
+}, false);
+
+window.addEventListener('contextmenu', function (e) { 
+    infoMenu.style.opacity = "1";
+    infoMenu.style.transform = "scale(1)";
+    infoMenu.style.left = e.pageX + 'px';
+    infoMenu.style.top = e.pageY + 'px';
+    e.preventDefault(); 
+  }, false);
+
+// Right-click specs
+
+let darkSupport = document.getElementById('darkSupport');
+let mobileSupport = document.getElementById('mobileSupport');
+let responsive = document.getElementById('responsive');
+let interactive = document.getElementById('interactive');
+
+button1.oncontextmenu = function () {
+    darkSupport.style.backgroundColor = "var(--green)";
+    mobileSupport.style.backgroundColor = "var(--green)";
+    responsive.style.backgroundColor = "var(--green)";
+    interactive.style.backgroundColor = "var(--red)";
+}
+
+button2.oncontextmenu = function () {
+    darkSupport.style.backgroundColor = "var(--green)";
+    mobileSupport.style.backgroundColor = "var(--yellow)";
+    responsive.style.backgroundColor = "var(--yellow)";
+    interactive.style.backgroundColor = "var(--green)";
+}
+
+button3.oncontextmenu = function () {
+    darkSupport.style.backgroundColor = "var(--green)";
+    mobileSupport.style.backgroundColor = "var(--yellow)";
+    responsive.style.backgroundColor = "var(--red)";
+    interactive.style.backgroundColor = "var(--yellow)";
+}
+
+button4.oncontextmenu = function () {
+    darkSupport.style.backgroundColor = "var(--green)";
+    mobileSupport.style.backgroundColor = "var(--yellow)";
+    responsive.style.backgroundColor = "var(--red)";
+    interactive.style.backgroundColor = "var(--green)";
+}
+
+button5.oncontextmenu = function () {
+    darkSupport.style.backgroundColor = "var(--yellow)";
+    mobileSupport.style.backgroundColor = "var(--green)";
+    responsive.style.backgroundColor = "var(--red)";
+    interactive.style.backgroundColor = "var(--green)";
+}
+
+button6.oncontextmenu = function () {
+    darkSupport.style.backgroundColor = "var(--green)";
+    mobileSupport.style.backgroundColor = "var(--green)";
+    responsive.style.backgroundColor = "var(--green)";
+    interactive.style.backgroundColor = "var(--red)";
+}
+
+button7.oncontextmenu = function () {
+    darkSupport.style.backgroundColor = "var(--green)";
+    mobileSupport.style.backgroundColor = "var(--green)";
+    responsive.style.backgroundColor = "var(--green)";
+    interactive.style.backgroundColor = "var(--red)";
 }
