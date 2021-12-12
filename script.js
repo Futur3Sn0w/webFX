f4451b0e325c9046780862342c3ba9c7b5646b68
Adam (Futur3Sn0w) <futur3sn0w@gmail.com>
2021-12-11 20:07:53 -0500

    aight i did it :tongue:

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Vars
 
 let navDial = document.getElementById('navDial');
-let showDial = document.getElementById('navActivator');
 
 let button1 = document.getElementById('pageBtn1');
 let button2 = document.getElementById('pageBtn2');
@@ -15,127 +14,115 @@ let button = document.querySelector('.pnBtn');
 
 let vwprt = document.getElementById('iViewport');
 
-let infoMenu = document.getElementById('infoMenu');
-
-// NavDial Buttons
-
-navDial.onclick = function () {
-    navDial.style.opacity = "0";
-    navDial.style.transform = "scale(0) translateY(50%)"
-}
-
-showDial.onclick = function () {
-    navDial.style.opacity = "1";
-    navDial.style.transform = "scale(1) translateY(0)"
-}
-
-button1.onclick = function() {
-    vwprt.src = "subpages/page1.html";
-
-    document.getElementById('navActivator').setAttribute("data-pageNo", "1");
-}
-
-button2.onclick = function() {
-    vwprt.src = "subpages/page2.html";
-
-    document.getElementById('navActivator').setAttribute("data-pageNo", "2");
-}
-
-button3.onclick = function() {
-    vwprt.src = "subpages/page3.html";
-
-    document.getElementById('navActivator').setAttribute("data-pageNo", "3");
-}
-
-button4.onclick = function() {
-    vwprt.src = "subpages/page4.html";
-
-    document.getElementById('navActivator').setAttribute("data-pageNo", "4");
-}
-
-button5.onclick = function() {
-    vwprt.src = "subpages/page5.html";
-
-    document.getElementById('navActivator').setAttribute("data-pageNo", "5");
-}
-
-button6.onclick = function() {
-    vwprt.src = "subpages/page6.html";
-
-    document.getElementById('navActivator').setAttribute("data-pageNo", "6");
-}
-
-button7.onclick = function() {
-    vwprt.src = "subpages/page7.html";
-
-    document.getElementById('navActivator').setAttribute("data-pageNo", "7");
-}
-
-// Right-click menu
-
-window.addEventListener('click', function () {
-    infoMenu.style.opacity = "0";
-    infoMenu.style.transform = "scale(0)";
-}, false);
-
-window.addEventListener('contextmenu', function (e) { 
-    infoMenu.style.opacity = "1";
-    infoMenu.style.transform = "scale(1)";
-    infoMenu.style.left = e.pageX + 'px';
-    infoMenu.style.top = e.pageY + 'px';
-    e.preventDefault(); 
-  }, false);
-
-// Right-click specs
+let scBtn = document.getElementById('scBtn');
+let sctTitle = document.getElementById('sctTitle');
 
 let darkSupport = document.getElementById('darkSupport');
 let mobileSupport = document.getElementById('mobileSupport');
 let responsive = document.getElementById('responsive');
 let interactive = document.getElementById('interactive');
 
-button1.oncontextmenu = function () {
+// NavDial Buttons
+
+window.onload = function () {
     darkSupport.style.backgroundColor = "var(--green)";
     mobileSupport.style.backgroundColor = "var(--green)";
     responsive.style.backgroundColor = "var(--green)";
     interactive.style.backgroundColor = "var(--red)";
 }
 
-button2.oncontextmenu = function () {
+button1.onclick = function() {
+    vwprt.src = "subpages/page1.html";
+
+    scBtn.innerHTML = button1.getAttribute('data-pageName');
+    sctTitle.innerHTML = button1.getAttribute('data-pageName');
+
+    document.body.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--my1');
+
+    darkSupport.style.backgroundColor = "var(--green)";
+    mobileSupport.style.backgroundColor = "var(--green)";
+    responsive.style.backgroundColor = "var(--green)";
+    interactive.style.backgroundColor = "var(--red)";
+}
+
+button2.onclick = function() {
+    vwprt.src = "subpages/page2.html";
+
+    scBtn.innerHTML = button2.getAttribute('data-pageName');
+    sctTitle.innerHTML = button2.getAttribute('data-pageName');
+
+    document.body.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--my2');
+
     darkSupport.style.backgroundColor = "var(--green)";
     mobileSupport.style.backgroundColor = "var(--yellow)";
     responsive.style.backgroundColor = "var(--yellow)";
     interactive.style.backgroundColor = "var(--green)";
 }
 
-button3.oncontextmenu = function () {
+button3.onclick = function() {
+    vwprt.src = "subpages/page3.html";
+
+    scBtn.innerHTML = button3.getAttribute('data-pageName');
+    sctTitle.innerHTML = button3.getAttribute('data-pageName');
+
+    document.body.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--my3');
+
     darkSupport.style.backgroundColor = "var(--green)";
     mobileSupport.style.backgroundColor = "var(--yellow)";
     responsive.style.backgroundColor = "var(--red)";
     interactive.style.backgroundColor = "var(--yellow)";
 }
 
-button4.oncontextmenu = function () {
+button4.onclick = function() {
+    vwprt.src = "subpages/page4.html";
+
+    scBtn.innerHTML = button4.getAttribute('data-pageName');
+    sctTitle.innerHTML = button4.getAttribute('data-pageName');
+
+    document.body.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--my4');
+
     darkSupport.style.backgroundColor = "var(--green)";
     mobileSupport.style.backgroundColor = "var(--yellow)";
     responsive.style.backgroundColor = "var(--red)";
     interactive.style.backgroundColor = "var(--green)";
 }
 
-button5.oncontextmenu = function () {
+button5.onclick = function() {
+    vwprt.src = "subpages/page5.html";
+
+    scBtn.innerHTML = button5.getAttribute('data-pageName');
+    sctTitle.innerHTML = button5.getAttribute('data-pageName');
+
+    document.body.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--my5');
+
     darkSupport.style.backgroundColor = "var(--yellow)";
     mobileSupport.style.backgroundColor = "var(--green)";
     responsive.style.backgroundColor = "var(--red)";
     interactive.style.backgroundColor = "var(--green)";
 }
 
-button6.oncontextmenu = function () {
+button6.onclick = function() {
+    vwprt.src = "subpages/page6.html";
+
+    scBtn.innerHTML = button6.getAttribute('data-pageName');
+    sctTitle.innerHTML = button6.getAttribute('data-pageName');
+
+    document.body.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--my6');
+
     darkSupport.style.backgroundColor = "var(--green)";
     mobileSupport.style.backgroundColor = "var(--green)";
     responsive.style.backgroundColor = "var(--green)";
     interactive.style.backgroundColor = "var(--red)";
 }
 
-button7.oncontextmenu = function () {
+button7.onclick = function() {
+    vwprt.src = "subpages/page7.html";
+
+    scBtn.innerHTML = button7.getAttribute('data-pageName');
+    sctTitle.innerHTML = button7.getAttribute('data-pageName');
+
+    document.body.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--my7');
+
     darkSupport.style.backgroundColor = "var(--green)";
     mobileSupport.style.backgroundColor = "var(--green)";
     responsive.style.backgroundColor = "var(--green)";
